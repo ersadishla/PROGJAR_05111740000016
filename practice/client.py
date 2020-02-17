26ddaba3f24355338ed484679b563adee852aaeb
ersadishla <ersadishlahuddin@gmail.com>
2020-02-17 17:29:08 +0700

    add master and practice file

---
+++ practice/client.py
@@ -0,0 +1,36 @@
+import sys
+import socket
+import random
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+port_number = int(input("input port number : "))
+server_address = ('127.0.0.1', port_number)
+try:
+    print(f"connecting to {server_address}")
+    sock.connect(server_address)
+except:
+    print("port not found")
+    port_list = [31001, 31002, 31003]
+    server_address = ('127.0.0.1', random.choice(port_list))
+    print(f"connecting to {server_address}")
+    sock.connect(server_address)
+
+try:
+    # Send data
+    message = input("input text messages : ")
+    # message = 'PEMROGRAMAN JARINGAN TEKNIK INFORMATIKA'
+    print(f"sending {message}")
+    sock.sendall(message.encode())
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print(f"{data}")
+finally:
+    print("closing")
+    sock.close()
---
+++ practice/server.py
@@ -0,0 +1,39 @@
+import sys
+import socket
+import _thread
+
+def initSocket( port_number ):
+    server_address = ('127.0.0.1', port_number)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f"start server on {server_address[0]} port {server_address[-1]}")
+    sock.bind(server_address)
+    sock.listen(1)
+    while True:
+        print(f"waiting for connection {server_address[-1]}")
+        connection, client_address = sock.accept()
+        print(f"connection from {client_address}")
+        # Receive the data in small chunks and retransmit it
+        text = ''
+        while True:
+            data = connection.recv(16)
+            text += data.decode()
+            print(f"received {data}")
+            if data:
+                print("sending back data")
+                connection.sendall(data)
+            else:
+                print(text)
+                print(f"no more data from {client_address}")
+                break
+        # Clean up the connection
+        connection.close()
+
+try:
+    _thread.start_new_thread( initSocket, (31001, ) )
+    _thread.start_new_thread( initSocket, (31002, ) )
+    _thread.start_new_thread( initSocket, (31003, ) )
+except:
+    print("Error: unable to start thread")
+
+while True:
+   pass
